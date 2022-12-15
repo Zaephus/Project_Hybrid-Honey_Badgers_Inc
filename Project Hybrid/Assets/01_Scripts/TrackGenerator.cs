83c35b0b4706a6155329df809f759ab9dcf1374c
Marc Rozendaal <rozendaalmarc@gmail.com>
2022-12-15 12:34:41 +0100

    Finished adding track generation

--- Project Hybrid/Assets/01_Scripts/TrackGenerator.cs
+++ Project Hybrid/Assets/01_Scripts/TrackGenerator.cs
@@ -4,91 +4,64 @@ using UnityEngine;
 
 public class TrackGenerator : MonoBehaviour {
 
-    [SerializeField, Range(1, 20)]
-    private int amountX = 1;
-    [SerializeField, Range(1, 20)]
-    private int amountY = 1;
+    [SerializeField, Range(10, 40)]
+    private int amountX = 15;
+    [SerializeField, Range(10, 40)]
+    private int amountY = 15;
 
     [SerializeField]
     private GameObject trackPrefab;
 
-    private List<List<Track>> tracks = new List<List<Track>>();
+    private List<TrackRow> rows = new List<TrackRow>();
 
-    private void OnValidate() {
-
-        Generate();
-
-        Debug.Log(tracks.Count);
-
-        // if(tracks.Count != 0) {
-
-        //     if(tracks.Count * tracks[0].Count > amountX * amountY) {
-
-        //         for(int i = 0; i < tracks.Count; i++) {
-        //             for(int j = 0; j < tracks[i].Count; j++) {
-
-        //                 if(tracks[i][j].transform.position.x > amountX || tracks[i][j].transform.position.y > amountY) {
-        //                     Track trackToRemove = tracks[i][j];
-        //                     tracks[i].Remove(trackToRemove);
-        //                     trackToRemove.DestroySelf();
-        //                 }
-
-        //             }
-
-        //             if(tracks[i].Count == 0) {
-        //                 tracks.RemoveAt(i);
-        //             }
-
-        //         }
-
-        //     }
-
-        // }
-        // else {
-
-        //     for(int x = 0; x < amountX; x++) {
-
-        //         if(x >= tracks.Count) {
-        //             tracks.Add(new List<Track>());
-        //         }
-
-        //         for(int z = 0; z < amountY; z++) {
-
-        //             if(x < tracks.Count && z < tracks[0].Count) {
-        //                 continue;
-        //             }
-
-        //             GameObject obj = Instantiate(trackPrefab, new Vector3(x, 0, z), Quaternion.identity, transform);
-        //             tracks[x].Add(obj.GetComponent<Track>());
-
-        //         }
-        //     }
-
-        // }
-
-    }
-
-    private void Generate() {
-        if(amountX > tracks.Count) {
-            for(int i = tracks.Count; i < amountX; i++) {
-                tracks.Add(new List<Track>());
+    public void Generate() {
+        if(amountX > rows.Count) {
+            for(int i = rows.Count; i < amountX; i++) {
+                rows.Add(new TrackRow());
 
                 for(int j = 0; j < amountY; j++) {
                     GameObject obj = Instantiate(trackPrefab, new Vector3(i, 0, j), Quaternion.identity, transform);
-                    tracks[i].Add(obj.GetComponent<Track>());
+                    rows[i].tracks.Add(obj.GetComponent<Track>());
                 }
             }
         }
-        else if(amountX < tracks.Count) {
-            ResetTracks();
+        else if(amountX < rows.Count) {
+            for(int i = amountX; i < rows.Count; i++) {
+                for(int j = 0; j < rows[i].tracks.Count; j++) {
+                    DestroyImmediate(rows[i].tracks[j].gameObject);
+                }
+                rows.RemoveAt(i);
+            } 
         }
+
+        for(int i = 0; i < rows.Count; i++) {
+
+            if(amountY > rows[i].tracks.Count) {
+                for(int j = rows[i].tracks.Count; j < amountY; j++) {
+                    GameObject obj = Instantiate(trackPrefab, new Vector3(i, 0, j), Quaternion.identity, transform);
+                    rows[i].tracks.Add(obj.GetComponent<Track>());
+                }
+            }
+            else if(amountY < rows[i].tracks.Count) {
+                for(int j = amountY; j < rows[i].tracks.Count; j++) {
+                    DestroyImmediate(rows[i].tracks[j].gameObject);
+                    rows[i].tracks.RemoveAt(j);
+                }
+            }
+
+            if(rows[i].tracks.Count == 0) {
+                rows.RemoveAt(i);
+            }
+
+        }
+
     }
 
 
-    private void ResetTracks() {
-        foreach(Transform child in transform) {
-            DestroyImmediate(child.gameObject);
-            tracks.Clear();
+    public void ResetTracks() {
+        for(int i = transform.childCount - 1; i >= 0; i--) {
+            DestroyImmediate(transform.GetChild(i).gameObject);
+            rows.Clear();
         }
     }
 
---
+++ Project Hybrid/Assets/01_Scripts/TrackRow.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TrackRow {
+
+    [SerializeField]
+    public List<Track> tracks = new List<Track>();
+
+}
